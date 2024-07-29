18ec9e1c1b2d7784a2c01fc448c0a642cf708317
youling4438 <youling0106@163.com>
2024-07-29 21:42:23 +0800

    update config for build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { TimeLineComponent } from './time-line/time-line.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'edward-life';
+  title = 'Edward Life';
 }
